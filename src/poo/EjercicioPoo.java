5b5f345d35bd9e07b31323a388748dfd13a3dfdd
s109e10 <mi.a9786@hotmail.es>
2019-09-07 11:54:13 -0500

    Explicación POO - Sobrecarga

---
+++ src/poo/EjercicioPoo.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package poo;
+
+/**
+ *
+ * @author s109e10
+ */
+public class EjercicioPoo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Perro miPerro;
+        miPerro = new Perro();
+        
+        Perro miPerro2 = new Perro("firu");
+        Perro miPerro3 = new Perro("firu", "pinshcer", 1.50);
+    }
+    
+}
