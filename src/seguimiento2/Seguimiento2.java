1a47dee44bf5dab62bbc3d412abf07c675022409
s109e10 <mi.a9786@hotmail.es>
2019-09-07 09:55:37 -0500

    resolviendo seguimiento 2 hasta el punto tres

--- src/seguimiento2/Seguimiento2.java
+++ src/seguimiento2/Seguimiento2.java
@@ -15,7 +15,48 @@ public class Seguimiento2 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        //Primer punto
+            // numerosUnoACien();
+        //Segundo punto
+            // numerosCienAUno();
+        
+        //Tercer punto
+        numerosPares();
+    }
+
+    private static void numerosUnoACien() {
+        
+        int[] numeros = new int[100];
+        System.out.println("Números del 1 al 100");
+        for (int i = 0; i < numeros.length; i++) {
+            
+            numeros[i] = i+1;
+            System.out.println(numeros[i]);
+            
+        }
+    }
+
+    private static void numerosCienAUno() {
+        
+        System.out.println("Números del 100 al 1");
+        
+            for(int i = 100; i > 0; i--)
+        {
+            System.out.println(i);
+        }
+    }
+
+    private static void numerosPares() {
+        
+        for (int i = 1; i <= 150; i++) {
+            
+            if(i % 2 == 0)
+            {
+                System.out.println(i);
+            }
+            
+        }
     }
     
 }
