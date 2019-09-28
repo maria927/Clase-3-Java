d9d3cc719fdbbc909174976f10ab81d7d43fec86
s109e10 <mi.a9786@hotmail.es>
2019-09-28 10:01:32 -0500

    wrapper clase 6

--- src/exceptions/EjemploExcepciones.java
+++ src/exceptions/EjemploExcepciones.java
@@ -5,6 +5,9 @@
  */
 package exceptions;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author s109e10
@@ -16,23 +19,59 @@ public class EjemploExcepciones {
      */
     public static void main(String[] args) {
        
-        convertirNumeros();
-        System.out.println("Mensaje despues de convertir números");
+        try {
+            //convertirNumeros();
+            //lanzarExcepcion();
+            //lanzarExcepcionConThrows();
+            
+            ejecucionDeDatos();
+            System.out.println("Mensaje despues de convertir números");
+        } catch (Exception ex) {
+            System.out.println("Capturando la excepcion desde el main");
+            System.out.println(ex);
+        }
         
     }
     
     public static void convertirNumeros()
     {
         try {
-              Integer wrapperInteger = new Integer("hola mundo");  
+              Integer wrapperInteger = new Integer("hola mundo");
+              System.out.println("Esto se ejecuta o no");
         } catch (Exception e) {
             
-            System.out.println("Ingresa al catch");
+            System.out.println("Ingresa al catch "+e);
+        } finally 
+        {
+            System.out.println("Ejecucion en el finally");
+        
         }
-
-        
-    
-        
+ 
     }
     
+    
+    //Con trow
+    public static void lanzarExcepcion()
+    {
+        
+        try {
+            System.out.println("Inicio para lanzar una excepción");
+            throw new Exception("Esta es la excepción lanzada");
+        } catch (Exception ex) {
+            System.out.println("Capturar la excepción "+ex);
+        }
+    }
+    
+    //Con throws
+    
+    public static void lanzarExcepcionConThrows() throws Exception
+    {
+       throw new Exception("Esta es la excepción lanzada desde el método con throws");
+    }
+
+    public static void ejecucionDeDatos() throws Exception
+    {
+          lanzarExcepcionConThrows();
+    }
 }
+
---
+++ src/exceptions/MenuException.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exceptions;
+
+/**
+ *
+ * @author s109e10
+ */
+public class MenuException extends Exception{
+    
+    public MenuException()
+    {
+       super("Error al ingresar la opción en el menu");
+    }
+}
